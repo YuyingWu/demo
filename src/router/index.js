9d1609eae3e5a0d164c24fb812e6e5a4947d26e9
Yuying Wu <wuyuying1128@gmail.com>
2017-03-04 21:36:32 +0800

    feat: router lazyloading, Resume and Lab js files lazyloading

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,22 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home/Home';
-import Resume from '@/components/Resume/Resume';
-import Lab from '@/components/Lab/Lab';
 
 Vue.use(Router);
+
+/*eslint-disable */
+const Resume = (resolve) => {
+  require.ensure(['@/components/Resume/Resume'], () => {
+    resolve(require('@/components/Resume/Resume'));
+  });
+};
+
+const Lab = (resolve) => {
+  require.ensure(['@/components/Lab/Lab'], () => {
+    resolve(require('@/components/Lab/Lab'));
+  });
+};
+/*eslint-enable */
 
 export default new Router({
   routes: [
